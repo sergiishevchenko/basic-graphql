292b9bf3b6d935fc2e796c1f14c31d37bbb6e00f
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-11-05 15:57:09 +0100

    added lists

--- server/app.js
+++ server/app.js
@@ -5,11 +5,14 @@ const schema = require('../schema/schema');
 const app = express();
 const PORT = 3005;
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true,
-}));
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true
+    })
+);
 
-app.listen(PORT, err => {
-  err ? console.log(err) : console.log('Server started!');
+app.listen(PORT, (err) => {
+    err ? console.log(err) : console.log('Server started!');
 });
