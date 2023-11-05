2d5a00742ad84d7ab368bd413f3b9a1ad06ff241
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-11-05 18:41:28 +0100

    added mongoose connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3005;
+
+mongoose.connect('mongodb://serg:password@ds163835.mlab.com:02332/graphql-db', {
+    useMongoClient: true
+});
 
 app.use(
     '/graphql',
@@ -12,6 +17,10 @@ app.use(
         graphiql: true
     })
 );
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', (err) => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
 
 app.listen(PORT, (err) => {
     err ? console.log(err) : console.log('Server started!');
