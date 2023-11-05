91cb40f9b6129873e49eec9dd6f86162b55f9143
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-11-05 18:02:59 +0100

    data for db

--- schema/schema.js
+++ schema/schema.js
@@ -43,7 +43,7 @@ const movies = [
 ];
 
 const directors = [
-	{ id: '1', name: "Quentin Tarantino", age: 55 },
+  { id: '1', name: "Quentin Tarantino", age: 55 },
   { id: '2', name: "Michael Radford", age: 72 },
   { id: '3', name: "James McTeigue", age: 51 },
   { id: '4', name: "Guy Ritchie", age: 50 },
