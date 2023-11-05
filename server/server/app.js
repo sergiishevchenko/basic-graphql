94c21c5ceb9476d0d3a7b884f0b5ef5e2312b442
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-11-05 23:38:05 +0100

    cors

--- server/server/app.js
+++ server/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
@@ -9,6 +10,8 @@ const PORT = 3005;
 mongoose.connect('mongodb://serg:password@ds163835.mlab.com:02332/graphql-db', {
     useMongoClient: true
 });
+
+app.use(cors());
 
 app.use(
     '/graphql',
