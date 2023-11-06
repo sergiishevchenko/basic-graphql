492de145c18ab5037d581579629d4e58590c43a2
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-11-06 10:25:12 +0100

    updateMovies

--- application/src/components/MoviesForm/MoviesFormHoc.js
+++ application/src/components/MoviesForm/MoviesFormHoc.js
@@ -1,6 +1,36 @@
 import { withStyles } from '@material-ui/core/styles';
 import { compose } from 'recompose';
+import { graphql } from 'react-apollo';
+
+import { addMovieMutation, updateMovieMutation } from './mutations';
+import { moviesQuery } from '../MoviesTable/queries';
+import { directorsQuery } from './queries';
 
 import { styles } from './styles';
 
-export default compose(withStyles(styles));
+const withGraphQL = compose(
+    graphql(addMovieMutation, {
+        props: ({ mutate }) => ({
+            addMovie: movie =>
+                mutate({
+                    variables: movie,
+                    refetchQueries: [{ query: moviesQuery }]
+                })
+        })
+    }),
+    graphql(updateMovieMutation, {
+        props: ({ mutate }) => ({
+            updateMovie: movie =>
+                mutate({
+                    variables: movie,
+                    refetchQueries: [{ query: moviesQuery }]
+                })
+        })
+    })
+);
+
+export default compose(
+    withStyles(styles),
+    withGraphQL,
+    graphql(directorsQuery)
+);
